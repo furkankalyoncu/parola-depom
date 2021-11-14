0071c0e10f78196fdebcf34386b76302334ccd78
furkankalyoncu <furkankalyoncu2000@hotmail.com>
2021-11-14 21:58:50 +0300

    Yeni bilgi ekleme, js iyileştirmeleri ve kopyalama

---
+++ js/core.js
@@ -0,0 +1,106 @@
+getCookie = (name = 'paroladepom') => {
+    const getCookie = Cookies.get(name)
+    let data = []
+    getCookie !== undefined ? data = JSON.parse(getCookie) : false
+    return data || [{}]
+}
+
+findPlatform = (platform) => {
+    const data = getCookie()
+    return data.find(d => d.platform == platform) || false
+}
+
+findById = (id) => {
+    const data = getCookie()
+    return data.find(d => d.id == id) || false
+}
+
+getRandomId = (min = 1121, max = 999999) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const num = Math.floor(Math.random() * (max - min + 1) + min);
+    return findById(num) || num
+}
+
+setCookie = (value, name = 'paroladepom') => {
+    let id = getRandomId()
+    console.log('value ', value);
+    const format = value.map(d => {
+        d.id = id
+        d.platform = d.platform
+        d.name = d.name
+        d.password = d.password
+    })
+    Cookies.set(name, value, { expires: 3500 })
+}
+
+updateCookie = (data) => {
+    var currentCookies = getCookie()
+    currentCookies.push(data)
+    setCookie(currentCookies)
+    return true
+}
+
+listAll = () => {
+    const data = getCookie()
+    var list = $('#list')
+    if (data.length > 0) {
+        for (let i = 0; i < data.length; i++) {
+            list.append(`<div class="max-w-sm mx-auto inline-flex">
+        <div class="flex flex-col">
+            <div class="bg-white border border-white shadow-lg  rounded-3xl p-4 m-4">
+                <div class="flex-none sm:flex">
+                    <div class="flex-auto sm:ml-5 justify-evenly">
+                        <div class="flex items-center justify-between sm:mt-2">
+                            <div class="flex items-center">
+                                <div class="flex flex-col">
+                                    <div
+                                        class="w-full flex-none text-lg text-gray-800 font-bold leading-none pb-3 flex">
+                                        ${data[i].platform}
+                                        <div class="pl-3" id="deletePass_123123"><svg
+                                                xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer"
+                                                fill="none" viewBox="0 0 26 26" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                            </svg></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <h3 class="font-medium">Kullanıcı Adı</h3>
+                        <p class="contents" id="username_${data[i].id}">${data[i].name}</p>
+                        <div id="copy" name="copy_${data[i].id}_name" class="contents">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block cursor-pointer"
+                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                        </svg>
+                        </div>
+                        <div>
+                            <h3 class="font-medium">Parola</h3>
+                            <p class="contents" id="password_${data[i].id}">${data[i].password}</p>
+                            <div id="copy" name="copy_${data[i].id}_password" class="contents">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block cursor-pointer"
+                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                            </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`)
+        }
+    } else {
+        list.append(`<ul class="flex flex-wrap flex-col justify-center items-center m-auto subpixel-antialiased">
+        <li>Uyarılar eklenecek</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+      </ul>`)
+    }
+}
---
+++ js/ui.js
@@ -0,0 +1,42 @@
+$("#newButton").click(function () {
+    $('#newForm').toggle();
+    $(this).text(function (i, text) {
+        return text === "Kapat" ? "Yeni" : "Kapat";
+    })
+});
+
+$("#new").click(function () {
+    event.preventDefault();
+    const formData = $('#form').serializeArray()
+    console.log(formData);
+    const platform = formData[0].value;
+    const username = formData[1].value;
+    const password = formData[2].value;
+    const total = { "platform": platform, "name": username, "password": password };
+    updateCookie(total);
+});
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $(document).on('click', '#copy', function () {
+            const name = this.attributes.name.value;
+            const getId = name.split('_');
+            const find = findById(getId[1]);
+            console.log(find);
+            navigator.clipboard.writeText(find[getId[2]]).then(function () {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 2000
+                  })
+                  Toast.fire({
+                    type: 'success',
+                    title: 'Kopyalandı'
+                  })
+            }, function (err) {
+                console.error('Async: Could not copy text: ', err);
+            });
+        });
+    }, 100);
+});
