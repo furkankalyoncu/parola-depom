75ab0dbd2350c066742b250aa8a5a08a5cbe9bee
furkankalyoncu <furkankalyoncu2000@hotmail.com>
2021-11-22 01:37:32 +0300

    Analytics fix

--- js/core.js
+++ js/core.js
@@ -148,7 +148,7 @@ listAll = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div class="pb-2.5">
+                            <div class="pb-2">
                             <h3 class="font-medium">Kullanıcı Adı</h3>
                             <p class="contents break-words whitespace-normal" id="username_${data[i].id}">${data[i].name}</p>
                             <div id="copy" name="copy_${data[i].id}_name" class="contents">
@@ -168,8 +168,8 @@ listAll = () => {
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
                                 </svg>
-                                </div>
-                                </div>
+                                    </div>
+                                        </div>
                             `
             if (data[i].url != '') {
                 raw += `
